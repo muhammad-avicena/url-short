cc01049559b5689e2b823795a616a34049f1d806
Muhammad Avicena <cenarahmant.dev@gmail.com>
2024-02-16 00:06:00 +0700

    feat: add rate-limiter middleware

--- middleware/index.ts
+++ middleware/index.ts
@@ -1,12 +1,14 @@
+import { Application } from "express";
 import bodyParserMiddleware from "./body-parser.middleware";
 import corsMiddleware from "./cors.middleware";
 import databaseMiddleware from "./database.middleware";
 import helmetMiddleware from "./helmet.middleware";
-import { Application } from "express";
 import morganMiddleware from "./morgan.middleware";
+import limiterMiddleware from "./rate-limiter.middleware";
 
 const middleware = (app: Application) => {
   morganMiddleware(app);
+  limiterMiddleware(app);
   bodyParserMiddleware(app);
   corsMiddleware(app);
   helmetMiddleware(app);
---
+++ middleware/rate-limiter.middleware.ts
@@ -0,0 +1,15 @@
+import { rateLimit } from "express-rate-limit";
+import { Application } from "express";
+
+const limiterMiddleware = (app: Application) => {
+  const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    limit: 10, // Limit each IP to 10 requests per `window` (here, per 1 minute).
+    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  });
+
+  app.use(limiter);
+};
+
+export default limiterMiddleware;
