feb17d5c88efc5427a2a8de237ea889897e46db0
Muhammad Avicena <cenarahmant.dev@gmail.com>
2024-02-16 16:28:31 +0700

    feat: modify cors and rate-limiter middleware

--- middleware/rate-limiter.middleware.ts
+++ middleware/rate-limiter.middleware.ts
@@ -4,7 +4,7 @@ import { Application } from "express";
 const limiterMiddleware = (app: Application) => {
   const limiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
-    limit: 10, // Limit each IP to 10 requests per `window` (here, per 1 minute).
+    limit: 50, // Limit each IP to 50 requests per `window` (here, per 1 minute).
     standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   });
